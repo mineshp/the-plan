5977822115cc7735d8b2a7ee6bc662e471363e4d
Minesh Patel <minesh.patel@net-a-porter.com>
2018-02-10 22:50:53 +0000

    Remove references to passport

--- server/app.js
+++ server/app.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const User = require('./mongodb/models/User');
 
 const app = express();
 const env = process.env.NODE_ENV || 'development';
@@ -13,9 +10,6 @@ const config = require('./config/config')[env];
 app.use(express.static(__dirname));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); // req.body params are available
-
-// app.use(passport.initialize());
-// app.use(passport.session());
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -35,10 +29,5 @@ if (dbType === 'mongodb') {
 }
 
 require('./config/routes')(app);
-
-// // passport config
-// passport.use(new LocalStrategy(User.authenticate()));
-// passport.serializeUser(User.serializeUser());
-// passport.deserializeUser(User.deserializeUser());
 
 module.exports = app;
